58941391f822938c4e2f9d853ac1535ee80d9928
Neha Ann Jacob <111440021+Neha-Ann-Jacob-Searce@users.noreply.github.com>
2022-11-07 12:57:35 +0530

    Add files via upload
    Testing File I/O

--- Cards/deck_test.go
+++ Cards/deck_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -10,4 +11,15 @@ func TestNewDeck(t *testing.T) {
 		// t.Error(d, "Expected deck sze of 52, but got ", len(d))
 		t.Errorf("Expected deck sze of 52, but got %v", len(d))
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards in deck, got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
